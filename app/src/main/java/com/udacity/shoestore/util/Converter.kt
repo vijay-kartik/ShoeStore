7cf7a5d0389dc51fb354c94ee1d1ed433b0db8b8
vijay-kartik <kartik.iit96@gmail.com>
2022-01-09 21:44:41 +0530

    Implement two-way binding in the user input fragment `fragment_add_shoe.xml`

--- app/src/main/java/com/udacity/shoestore/util/Converter.kt
+++ app/src/main/java/com/udacity/shoestore/util/Converter.kt
@@ -3,12 +3,12 @@ package com.udacity.shoestore.util
 import androidx.databinding.InverseMethod
 
 object Converter {
-    @InverseMethod("toLong")
+    @InverseMethod("toDouble")
     @JvmStatic fun toString(long: Double): String {
         return long.toString()
     }
 
-    @JvmStatic fun toLong(string: String): Double {
+    @JvmStatic fun toDouble(string: String): Double {
         return string.toDouble()
     }
 }
